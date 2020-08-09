41ef9f8fef0c5e01f045b6d96203dbc88f538fc6
kbuza <buza.work@gmail.com>
2020-08-10 00:52:13 +0300

    removed test click handler

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -2,20 +2,9 @@ import React from 'react';
 import './App.css';
 import { useTranslation } from 'react-i18next';
 
-const languages = ['ru_RU', 'uk_UA', 'en'];
-const clickCallback = (i18n) => {
-  const langIndex = languages.indexOf(i18n.language);
-  const nextLocale = langIndex === languages.length - 1 ? languages[0] : languages[langIndex + 1];
-  i18n.changeLanguage(nextLocale, () => moment && console.log(moment.locale()));
-};
-
 const App = () => {
-  const { t, i18n } = useTranslation();
-  return (
-    <h1 onClick={() => clickCallback(i18n)}>
-      <span>{t('hello.world')}</span>
-    </h1>
-  );
+  const { t } = useTranslation();
+  return <h1>{t('hello.world')}</h1>;
 };
 
 export default App;
