50dadafd547b5929037aed9beb7b1b89fa71c929
kbuza <buza.work@gmail.com>
2020-08-10 00:39:28 +0300

    added utils (i18next and moment)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { render } from 'react-dom';
 import App from './Containers/App/App';
+import './utils';
 
 const root = document.querySelector('#root');
 
-const template = <App />;
+const template = (
+  <Suspense fallback="heck">
+    <App />
+  </Suspense>
+);
 
 render(template, root);
