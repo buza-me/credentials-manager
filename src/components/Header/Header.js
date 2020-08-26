f241a55fd903b12e11de4d727a60a09f8da8420e
kbuza <buza.work@gmail.com>
2020-08-26 15:42:10 +0300

    created Header components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,33 @@
+import './Header.css';
+import React from 'react';
+import { Link as BrowserLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
+import Divider from '@material-ui/core/Divider';
+import { LOGO_TEXT } from 'Constants';
+import { LanguageSelector, ThemeSelector } from 'Components';
+
+export const Header = ({ children, isBasic = false }) => (
+  <section className='header__container'>
+    <div>
+      {!isBasic ? (
+        <Link
+          component={BrowserLink}
+          className='header__home-link'
+          to='/'
+          color='primary'
+          underline='none'
+        >
+          {LOGO_TEXT}
+        </Link>
+      ) : null}
+    </div>
+    <main className='header__button-container'>
+      {children}
+      {children ? (
+        <Divider orientation='vertical' className='header__button-container_divider' flexItem />
+      ) : null}
+      <LanguageSelector />
+      <ThemeSelector />
+    </main>
+  </section>
+);
