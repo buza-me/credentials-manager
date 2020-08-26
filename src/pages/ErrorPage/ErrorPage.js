56d4ff83d356f8de830dbc5cdf9d475cd6b675de
kbuza <buza.work@gmail.com>
2020-08-26 15:44:40 +0300

    added error page

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,4 +1,18 @@
+import './ErrorPage.css';
 import React from 'react';
-import { HiComponent } from 'Components';
+import { Trans } from 'react-i18next';
+import { Link as BrowserLink } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 
-export const ErrorPage = () => <HiComponent />;
+const renderLink = () => (
+  <Link component={BrowserLink} className='error-page__home-link' to='/' color='primary' />
+);
+
+export const ErrorPage = () => (
+  <section className='error-page__container'>
+    <Typography variant='h4' component='h1' className='error-page__header'>
+      <Trans i18nKey='error.pageNotFound' components={{ 1: renderLink() }} />
+    </Typography>
+  </section>
+);
