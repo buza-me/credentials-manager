108b45040bb3415108f7a571f72f78ed0f14fe48
kbuza <buza.work@gmail.com>
2020-08-26 17:58:57 +0300

    lowered page transition duration to 300ms

--- src/constants/common.constants.js
+++ src/constants/common.constants.js
@@ -2,4 +2,4 @@ export const LOGO_TEXT = 'PWM';
 export const API_URL = 'https://kbuza-credentials-api.herokuapp.com';
 export const USER_URL = `${API_URL}/user`;
 
-export const PAGE_TRANSITION_DURATION = 500;
+export const PAGE_TRANSITION_DURATION = 300;
