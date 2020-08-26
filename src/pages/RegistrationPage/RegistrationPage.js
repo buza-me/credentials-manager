c696d664c2068c7983bedd4e208166d630410a21
kbuza <buza.work@gmail.com>
2020-08-26 17:32:42 +0300

    finished registration page

--- src/pages/RegistrationPage/RegistrationPage.js
+++ src/pages/RegistrationPage/RegistrationPage.js
@@ -1,35 +1,81 @@
 import './RegistrationPage.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import IconButton from '@material-ui/core/IconButton';
+import Alert from '@material-ui/lab/Alert';
+import Snackbar from '@material-ui/core/Snackbar';
+
 import VisibilityTwoToneIcon from '@material-ui/icons/VisibilityTwoTone';
 import VisibilityOffTwoToneIcon from '@material-ui/icons/VisibilityOffTwoTone';
-import Button from '@material-ui/core/Button';
-import { Header } from 'Components';
+
+import { Header, Spinner } from 'Components';
 import { useTranslation } from 'react-i18next';
+import { USER_URL } from 'Constants';
 
 export const RegistrationPage = () => {
   const { t } = useTranslation();
   const [passwordInputType, setPasswordInputType] = useState('password');
   const [repeatPasswordInputType, setRepeatPasswordInputType] = useState('password');
+  const [isLoading, setIsLoading] = useState(false);
+  const [registrationStatus, setRegistrationStatus] = useState(null);
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const resetForm = () => {
+    setName('');
+    setEmail('');
+    setPassword('');
+    setRepeatPassword('');
   };
 
-  const renderEndAdornment = (typeVar, typeSetter) => (
+  useEffect(() => {
+    if (registrationStatus === 'success') resetForm();
+  }, [registrationStatus]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (isLoading) {
+      return;
+    }
+    try {
+      setIsLoading(true);
+
+      const response = await fetch(USER_URL, {
+        method: 'POST',
+        body: JSON.stringify({ name, email, password }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const userId = await response.json();
+
+      setIsLoading(false);
+
+      if (userId) {
+        setRegistrationStatus('success');
+        return;
+      }
+      throw new Error();
+    } catch (error) {
+      setIsLoading(false);
+      setRegistrationStatus('error');
+    }
+  };
+
+  const renderEndAdornment = (type, typeSetter) => (
     <InputAdornment position='end'>
-      <IconButton onClick={() => typeSetter(typeVar === 'password' ? 'text' : 'password')}>
-        {typeVar === 'password' ? (
+      <IconButton onClick={() => typeSetter(type === 'password' ? 'text' : 'password')}>
+        {type === 'password' ? (
           <VisibilityTwoToneIcon className='registration-page__form_reveal-button-icon' />
         ) : (
           <VisibilityOffTwoToneIcon className='registration-page__form_reveal-button-icon' />
@@ -45,8 +91,9 @@ export const RegistrationPage = () => {
       label: 'form.label.name',
       errorText: 'form.error.name',
       type: 'text',
-      onInput: (e) => setName(e.target.value),
-      endAdornment: null
+      onChange: (e) => setName(e.target.value),
+      endAdornment: null,
+      value: name
     },
     {
       isError: !!email && !/\S+@\S+\.\S+/.test(email),
@@ -54,8 +101,9 @@ export const RegistrationPage = () => {
       label: 'form.label.email',
       errorText: 'form.error.email',
       type: 'text',
-      onInput: (e) => setEmail(e.target.value),
-      endAdornment: null
+      onChange: (e) => setEmail(e.target.value),
+      endAdornment: null,
+      value: email
     },
     {
       isError: !!password && password.length < 8,
@@ -63,8 +111,9 @@ export const RegistrationPage = () => {
       label: 'form.label.password',
       errorText: 'form.error.password',
       type: passwordInputType,
-      onInput: (e) => setPassword(e.target.value),
-      endAdornment: renderEndAdornment(passwordInputType, setPasswordInputType)
+      onChange: (e) => setPassword(e.target.value),
+      endAdornment: renderEndAdornment(passwordInputType, setPasswordInputType),
+      value: password
     },
     {
       isError: !!password && !!repeatPassword && password !== repeatPassword,
@@ -72,15 +121,16 @@ export const RegistrationPage = () => {
       label: 'form.label.password.repeat',
       errorText: 'form.error.password.repeat',
       type: repeatPasswordInputType,
-      onInput: (e) => setRepeatPassword(e.target.value),
-      endAdornment: renderEndAdornment(repeatPasswordInputType, setRepeatPasswordInputType)
+      onChange: (e) => setRepeatPassword(e.target.value),
+      endAdornment: renderEndAdornment(repeatPasswordInputType, setRepeatPasswordInputType),
+      value: repeatPassword
     }
   ];
 
   const isInvalid =
     !name || !email || !password || !repeatPassword || inputsModel.some(({ isError }) => isError);
 
-  const renderInput = ({ isError, errorText, endAdornment, onInput, id, label, type }) => (
+  const renderInput = ({ isError, errorText, endAdornment, onChange, id, label, type, value }) => (
     <FormControl error={isError} key={id}>
       <InputLabel htmlFor={id}>{t(label)}</InputLabel>
       <Input
@@ -88,9 +138,10 @@ export const RegistrationPage = () => {
         id={id}
         type={type}
         endAdornment={endAdornment}
-        onInput={onInput}
+        onChange={onChange}
         color='primary'
         autoComplete='off'
+        value={value}
       />
       {isError ? <FormHelperText>{t(errorText)}</FormHelperText> : null}
     </FormControl>
@@ -108,11 +159,29 @@ export const RegistrationPage = () => {
             color='secondary'
             variant='contained'
             disableElevation
+            endIcon={isLoading ? <Spinner /> : null}
           >
             {t('link.registration')}
           </Button>
         </form>
       </section>
+      <Snackbar
+        className='registration-page__snackbar'
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={!!registrationStatus}
+        autoHideDuration={3000}
+        onClose={() => setRegistrationStatus(null)}
+      >
+        {registrationStatus ? (
+          <Alert
+            onClose={() => setRegistrationStatus(null)}
+            severity={registrationStatus}
+            variant='filled'
+          >
+            {t(`registration.${registrationStatus}`)}
+          </Alert>
+        ) : null}
+      </Snackbar>
     </main>
   );
 };
