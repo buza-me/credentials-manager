4e4dcdd774d0fbef4eac30dbeb1394ac82b8fc88
kbuza <buza.work@gmail.com>
2020-08-26 15:44:16 +0300

    added index page

--- src/pages/IndexPage/IndexPage.js
+++ src/pages/IndexPage/IndexPage.js
@@ -1,4 +1,46 @@
+import './IndexPage.css';
 import React from 'react';
-import { HiComponent } from 'Components';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { LOG_IN_ROUTE, REGISTRATION_ROUTE, LOGO_TEXT } from 'Constants';
+import { Header } from 'Components';
 
-export const IndexPage = () => <HiComponent />;
+export const IndexPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <section className='index-page__container'>
+      <Header isBasic />
+      <main className='index-page__content'>
+        <h1 className='index-page__logo'>{LOGO_TEXT}</h1>
+        <Typography variant='h4' component='h2' className='index-page__header'>
+          {t('index.welcome')}
+        </Typography>
+        <section className='index-page__links'>
+          <Button
+            component={Link}
+            className='index-page__link'
+            to={LOG_IN_ROUTE}
+            color='secondary'
+            variant='contained'
+            disableElevation
+          >
+            {t('link.login')}
+          </Button>
+          <Button
+            component={Link}
+            className='index-page__link'
+            to={REGISTRATION_ROUTE}
+            color='secondary'
+            variant='contained'
+            disableElevation
+          >
+            {t('link.registration')}
+          </Button>
+        </section>
+      </main>
+    </section>
+  );
+};
