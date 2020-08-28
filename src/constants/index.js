39a0dd64b7e6b5451585fdaf1ce16c24bd25b199
kbuza <buza.work@gmail.com>
2020-08-29 02:08:15 +0300

    added fresh constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 export * from './localStorage.constants';
+export * from './actionTypes.constants';
 export * from './eventNames.constants';
 export * from './common.constants';
 export * from './routes.constants';
--- src/constants/localStorage.constants.js
+++ src/constants/localStorage.constants.js
@@ -3,5 +3,5 @@ const { hostname } = window.location;
 export const NO_VALID_TOKEN_LOCALSTORAGE_KEY = `${hostname}_noValidToken`;
 export const JWT_TOKEN_LOCALSTORAGE_KEY = `${hostname}_JWTToken`;
 export const JWT_EXPIRATION_TIME_LOCALSTORAGE_KEY = `${hostname}_JWTExpirationTime`;
-export const PREFERRED_LANGUAGE_LOCALSTORAGE_KEY = `${hostname}_preferredLanguage`;
-export const PREFERRED_THEME_LOCALSTORAGE_KEY = `${hostname}_preferredTheme`;
+export const USER_PREFERENCES_LOCALSTORAGE_KEY = `${hostname}_userPreferences`;
+export const USER_ID_LOCALSTORAGE_KEY = `${hostname}_userId`;
