6863ebc889da12dc8c068d39d92d7f2d8ebe4cd6
kbuza <buza.work@gmail.com>
2020-08-29 02:14:24 +0300

    started working on a profile page

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,4 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+import Button from '@material-ui/core/Button';
 import { Page } from 'Containers';
+import { Header } from 'Components';
+import { LoginContext } from 'Contexts';
 
-export const ProfilePage = () => <Page />;
+export const ProfilePage = () => {
+  const { t } = useTranslation();
+  const { logOut } = useContext(LoginContext);
+  return (
+    <Page>
+      <Header>
+        <Button color='secondary' variant='contained' disableElevation onClick={logOut}>
+          {t('link.logout')}
+        </Button>
+      </Header>
+    </Page>
+  );
+};
