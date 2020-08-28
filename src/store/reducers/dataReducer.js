2324bf105a1b5a7db07dda70224ff53404bce840
kbuza <buza.work@gmail.com>
2020-08-29 02:20:24 +0300

    initiated reducers

--- src/store/reducers/dataReducer.js
+++ src/store/reducers/dataReducer.js
@@ -1,30 +1,15 @@
 /* eslint-disable no-underscore-dangle */
-import { WRITE_RECORDS, WRITE_RECORD, UPDATE_RECORD, DELETE_RECORD } from 'Constants';
-
 const initialState = {
   records: []
 };
 
-export default function dataReducer(state = initialState, { type, records, record, id }) {
+// eslint-disable-next-line no-unused-vars
+export default function dataReducer(state = initialState, { type, payload }) {
   const newState = { ...state };
 
   switch (type) {
-    case WRITE_RECORDS:
-      newState.records = records;
-      break;
-    case WRITE_RECORD:
-      newState.records.push(record);
-      break;
-    case UPDATE_RECORD:
-      newState.records = newState.records.map((recordFromState) =>
-        recordFromState._id === record._id ? record : recordFromState
-      );
-      break;
-    case DELETE_RECORD:
-      newState.records = newState.records.filter((recordFromState) => recordFromState._id !== id);
-      break;
     default:
-      return newState;
+      break;
   }
 
   return newState;
