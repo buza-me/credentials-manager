13be8f92aad92285d4b376b26997a5f35735186e
kbuza <buza.work@gmail.com>
2020-08-29 02:09:33 +0300

    changed the way i18n language change listener saves preferences

--- src/core/i18n.config.js
+++ src/core/i18n.config.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import detector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-import { PREFERRED_LANGUAGE_LOCALSTORAGE_KEY } from 'Constants';
+import { USER_PREFERENCES_LOCALSTORAGE_KEY } from 'Constants';
 import resources from './i18n';
 
 i18n
@@ -18,11 +18,18 @@ i18n
     }
   });
 
-i18n.on('languageChanged', (lang) =>
-  localStorage.setItem(PREFERRED_LANGUAGE_LOCALSTORAGE_KEY, lang)
-);
+const { parse, stringify } = JSON;
 
-const preferredLanguage = localStorage.getItem(PREFERRED_LANGUAGE_LOCALSTORAGE_KEY);
+i18n.on('languageChanged', (language) => {
+  const userPreferences = parse(localStorage.getItem(USER_PREFERENCES_LOCALSTORAGE_KEY) || '{}');
+  localStorage.setItem(
+    USER_PREFERENCES_LOCALSTORAGE_KEY,
+    stringify({ ...userPreferences, language })
+  );
+});
+
+const userPreferences = parse(localStorage.getItem(USER_PREFERENCES_LOCALSTORAGE_KEY) || '{}');
+const preferredLanguage = userPreferences.language;
 
 if (preferredLanguage) {
   i18n.changeLanguage(preferredLanguage);
