a46c97b856e47a40a2b23e4c16550b415a23b508
kbuza <buza.work@gmail.com>
2020-09-09 06:04:41 +0300

    modified server

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-// app.use(express.static(__dirname));
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/ping', (req, res) => res.send('pong'));
 
 app.get('*', (req, res) => {
-  const pathname = path.join(__dirname, 'dist', 'index.html');
+  const pathname = path.join(__dirname, 'dist', 'dist/index.html');
   console.log(pathname);
   res.sendFile(pathname);
 });
