1af466002e803e5f25ea3f4ec0874396f9896c34
kbuza <buza.work@gmail.com>
2020-09-09 05:23:51 +0300

    modified server

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
 
-const port = process.env.PORT || 8080;
+const publicPath = path.join(__dirname, '..', 'dist');
+const port = process.env.PORT || 3000;
+
 const app = express();
 
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('/ping', (req, res) => res.send('pong'));
+app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(port);
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is up at port ${port}!`);
+});
 
-console.log(`app started at port ${port}`);
+console.log(path.join(publicPath, 'index.html'));
