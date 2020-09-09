4a81f40f8deffe772653be2a7e17758a7d72cf5f
kbuza <buza.work@gmail.com>
2020-09-09 05:32:49 +0300

    modified server

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-const path = require('path');
 const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
 
-const publicPath = path.join(__dirname, '..', 'dist');
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(publicPath));
+app.use(favicon(path.join(__dirname, '/dist/favicon.ico')));
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/ping', (req, res) => res.send('pong'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is up at port ${port}!`);
+  console.log(`app started at port ${port}`);
+  console.log(`serve from: ${path.join(__dirname, 'dist', 'index.html')}`);
 });
-
-console.log(path.join(publicPath, 'index.html'));
