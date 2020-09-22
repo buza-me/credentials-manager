ab87fa6c7836b46f31bde56ae041981a867b572b
kbuza <buza.work@gmail.com>
2020-09-22 20:44:24 +0300

    fixed server error on page reload in production, which led to served html instead of JS and CSS

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
-// the __dirname is the current directory from where the script is running
-// app.use(express.static(__dirname));
-app.use('', express.static(path.join(__dirname, 'dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.get('*', async (req, res) => {
+  const fileName = req.originalUrl?.split('/')?.reverse()?.[0] || 'index.html';
+  const pathToFile = path.join(__dirname, 'dist', fileName);
+  fs.access(pathToFile, fs.constants.F_OK, (err) => {
+    if (!err) {
+      res.sendFile(pathToFile);
+    }
+  });
 });
 
 app.listen(port, () => {
